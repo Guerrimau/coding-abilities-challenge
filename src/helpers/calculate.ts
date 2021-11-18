80f03df48c080ffa41de722542b162a20dfe275d
Gerardo <gmayboca@gmail.com>
2021-11-18 00:28:48 -0700

    added for loop to iterate between the Numbers

--- src/helpers/calculate.ts
+++ src/helpers/calculate.ts
@@ -2,6 +2,17 @@ export default function calculate(expression: string) {
   const expressionCharacters = expression.split(" ");
   const expressionNumbers = expressionCharacters.filter(character => !isNaN(Number(character)));
   const expressionSymbols = expressionCharacters.filter(character => isNaN(Number(character)));
-  console.log({expressionCharacters, expressionNumbers, expressionSymbols});
+  
+  let symbolsIndex = expressionSymbols.length - 1;
+  let result = 0;
+
+  for (let index = 0; index < expressionNumbers.length; index++) {
+    if(index === 0) {
+      
+    }
+    
+  }
+
+  return result;
   
 }
