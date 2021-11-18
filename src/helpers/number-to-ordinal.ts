08e5ecdfb3dd3ed4dd959f192319b311c3002b59
Gerardo <gmayboca@gmail.com>
2021-11-17 23:44:34 -0700

    numberToOrdinal function with first validation

---
+++ src/helpers/number-to-ordinal.ts
@@ -0,0 +1,5 @@
+
+export default function numberToOrdinal(Number: number) {
+  if(Number === 0) return "0";
+
+}
--- src/pages/conding-abilities-challenge/index.tsx
+++ src/pages/conding-abilities-challenge/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import maskify from '../../helpers/maskify';
+import numberToOrdinal from '../../helpers/number-to-ordinal';
 
 export const CodingAbilitiesChallenge = () => {
 
   const maskedString = maskify("12345634324234322332");
-
-  console.log(maskedString);
+  const ordinalNumber = numberToOrdinal("2");
   
   return (
     <div>
