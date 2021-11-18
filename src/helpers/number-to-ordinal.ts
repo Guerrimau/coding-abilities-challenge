83d1c093a80bd2749b7dd51068ef00fda3b53d86
Gerardo <gmayboca@gmail.com>
2021-11-18 00:05:03 -0700

    added if sentence for basic exceptions

--- src/helpers/number-to-ordinal.ts
+++ src/helpers/number-to-ordinal.ts
@@ -1,5 +1,14 @@
 
 export default function numberToOrdinal(Number: number) {
-  if(Number === 0) return "0";
+  
+  if(Number === 0) {
+    return "0";
+  } else if(Number === 1) {
+    return "1st";
+  } else if(Number === 2) {
+    return "2nd";
+  } else if (Number === 3) {
+    return "3rd";
+  }
 
 }
