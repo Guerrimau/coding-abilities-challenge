412558a21191fcee349dda9996dfbfa8f6138408
Gerardo <gmayboca@gmail.com>
2021-11-18 13:39:26 -0700

    Moved classes to a single css file

--- src/components/ejercice-card.tsx
+++ src/components/ejercice-card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../styles/ejercice-card.css";
 
 interface IejerciceCard {
   title: string,
@@ -22,7 +23,7 @@ export const EjerciceCard = ({
       <h3 className="text-xl mb-2 font-medium text-purple-700">{title}</h3>
       <div
         style={{ width: "500px", height: "250px" }}
-        className="flex flex-col justify-between b py-10 px-8 bg-white rounded-xl shadow-md space-y-2">
+        className="ejercice__card">
         <span className="text-xs">Output:</span>
         <span className="text-center">
           {
@@ -33,12 +34,12 @@ export const EjerciceCard = ({
         </span>
         <div className="flex flex-row justify-between">
           <input
-            className="border py-2 px-3 text-grey-darkest w-9/12 focus:outline-none focus:shadow-outline"
+            className="ejercice__card--input"
             value={inputValues}
             onChange={onChange}
             type={inputType}/>
           <button
-            className="bg-purple-700 text-white px-4 py-2 rounded hover:bg-purple-800 transition"
+            className="ejercice__card--btn"
             onClick={onClick}>Ejecutar</button>
         </div>
       </div>
