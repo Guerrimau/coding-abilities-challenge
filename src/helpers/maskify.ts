7f89c12fa0e9a462ecc9789539a4bc49b070017f
Gerardo <gmayboca@gmail.com>
2021-11-17 23:36:07 -0700

    started for loop with first sentence to return the first character as normal

--- src/helpers/maskify.ts
+++ src/helpers/maskify.ts
@@ -2,9 +2,18 @@
 export default function maskify (cardNumber: string) {
   
   if(cardNumber.length < 7) return cardNumber;
-  
-  for (let char of cardNumber) {
-      console.log(char);
+
+  let maskedNumber = "";
+
+  for (let index = 0; index < cardNumber.length; index++) {
+    const char = cardNumber[index];
+    
+    if(index === 0) {
+      maskedNumber = maskedNumber + char;
+    }
+
   }
+
+  return maskedNumber;
 
 }
