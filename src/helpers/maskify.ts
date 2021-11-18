61ff3df929f558aa8bed95cae09a4a531e57eaeb
Gerardo <gmayboca@gmail.com>
2021-11-17 23:38:40 -0700

    lastFourIndex variable to idenfity the last 4 numbers of the string

--- src/helpers/maskify.ts
+++ src/helpers/maskify.ts
@@ -3,6 +3,7 @@ export default function maskify (cardNumber: string) {
   
   if(cardNumber.length < 7) return cardNumber;
 
+  const lastFourIndex = cardNumber.length - 5; // 4 digits + 1 extra to match with the array index
   let maskedNumber = "";
 
   for (let index = 0; index < cardNumber.length; index++) {
