eedd51f21afbf8134d815c12a54a0d52a3530a91
Gerardo <gmayboca@gmail.com>
2021-11-17 23:36:31 -0700

    imported maskify function into CodingAbilitiesChallenge page to test it

--- src/pages/conding-abilities-challenge/index.tsx
+++ src/pages/conding-abilities-challenge/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import maskify from '../../helpers/maskify';
 
 export const CodingAbilitiesChallenge = () => {
+
+  const maskedString = maskify("12345634324234322332");
+
+  console.log(maskedString);
+  
   return (
     <div>
       <h1>HolaMundo </h1>
