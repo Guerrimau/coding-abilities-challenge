ed39ba528311264172668d7e8a3bcfd4813a94b6
Gerardo <gmayboca@gmail.com>
2021-11-18 01:35:53 -0700

    added typescript

--- src/pages/conding-abilities-challenge/index.tsx
+++ src/pages/conding-abilities-challenge/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { CalculateSection } from '../../components/calculate-section';
 import { CardNumberMaskifySection } from '../../components/card-number-maskify-section';
 import { OrdinalNumbersSection } from '../../components/number-to-ordinal-section';
-import calculate from '../../helpers/calculate';
-import maskify from '../../helpers/maskify';
-import numberToOrdinal from '../../helpers/number-to-ordinal';
 
 export const CodingAbilitiesChallenge = () => {
   
